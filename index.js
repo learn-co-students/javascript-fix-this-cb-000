f3ed0c73dc3e9431548cb0f00034b15381f1ecb1
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-06-29 12:37:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+/*
+The way that I could undrestand how this code works:
+
+User clicks on [Make a Cake] link:
+1. we know we need to make a cake so we call #makeCake
+2. we need a way to update the status of our cake. We make a copy of #updateStatus that knows what is 'this' so it
+  can correctly change the status for us in for the right <div>.
+3. we call the new #updateStatus function that we just made and we pass it the new status which is "prep".
+4* The status has changed to ["prep"], only for a milli second.
+5. Now we want to call #mix function on this cake so #mix can change the stauts for us too. We set everything for #mix to work.
+  It needs a function so it can also change the status. Since we have a ready function that knows how to change status of this
+  particular cake we are passing the same function to that.
+6* In #mix we chnage the status to ["Mixing " + this.ingredients.join(", ").]
+7. After 2 seconds we call #bake function and let #bake function know how to update the status
+8* The status has changed to ["Baking at " + this.bakeTemp + " for " + this.bakeTime]
+9. After 2 seconds we call #cool function and let #cool function know how to update the status
+10* The status has changed to ["It has to cool! Hands off!"]
+11. After 2 seconds we call #decorete function that belongs to cake itself and let #decorete function know how
+  to update the status
+12* The status has changed to ["Decorating with " + this.topping + ". Ready to eat soon!"]
+13. After 2 seconds we call #updateCakeStatus and pass to it #serve function and let #serve function know how about the cake by passing
+  cake as 'this' to it. 
+14* The status has changed to ["Decorating with " + this.topping + ". Ready to eat soon!"]
+
+
+In all phases we are passing cake as 'this' with methods like call and apply.
+mix(updateCakeStatus) -> bake(updateCakeStatus) -> cool(updateCakeStatus) -> this.decorate(updateCakeStatus) -> updateCakeStatus(serve)
+this = cake               this = cake               this = cake               this = cake                         this = cake
+*/
+
 var cake = {
   name: "German Chocolate Cake",
   ingredients: ["eggs", "flour", "oil", "chocolate", "sugar", "butter"],
@@ -12,6 +42,14 @@ var cake = {
       updateFunction(serve.apply(this, ["Happy Eating!", this.customer]))
     }, 2000)
   }
+}
+function cool(updateFunction) {
+  var status = "It has to cool! Hands off!"
+  updateFunction(status)
+
+  setTimeout(() => {
+    this.decorate(updateFunction)
+  }, 2000)
 }
 
 var pie = {
