8fec6310250c531407e64cf457434b80be2e21ea
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-06 22:48:25 +0000

    Done.

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ var cake = {
   decorate: function(updateFunction) {
     var status = "Decorating with " + this.topping + ". Ready to eat soon!"
     updateFunction(status)
-    setTimeout(function() {
-      updateFunction(serve.apply(this, "Happy Eating!", this.customer))
+    setTimeout(() => {
+      serve.apply(this, ["Happy Eating!", this.customer])
     }, 2000)
   }
 }
@@ -24,17 +24,21 @@ var pie = {
 }
 
 function makeCake() {
-  var updateCakeStatus;
-  mix(updateCakeStatus)
+  var cakeNode = document.getElementById("cake")
+  var updateCakeStatus = updateStatus.bind(cakeNode)
+
+  mix.call(cake,updateCakeStatus)
 }
 
 function makePie() {
-  var updatePieStatus;
-  mix(updatePieStatus)
+  var pieNode = document.getElementById("pie")
+  var updatePieStatus = updateStatus.bind(pieNode)
+  pie.decorate = cake.decorate.bind(pie)
+  mix.call(pie,updatePieStatus)
 }
 
 function updateStatus(statusText) {
-  this.getElementsByClassName("status")[0].innerText = statusText
+  document.getElementsByClassName("status")[0].innerText = statusText
 }
 
 function bake(updateFunction) {
@@ -42,26 +46,38 @@ function bake(updateFunction) {
   setTimeout(function() {
     cool(updateFunction)
   }, 2000)
+  updateFunction(status)
 }
-
 function mix(updateFunction) {
   var status = "Mixing " + this.ingredients.join(", ")
+  var item = (this.name.includes("Pie")) ? pie : cake
   setTimeout(function() {
-    bake(updateFunction)
-  }, 2000)
+    bake.call(item, updateFunction)
+    }, 2000)
   updateFunction(status)
 }
 
 function cool(updateFunction) {
   var status = "It has to cool! Hands off!"
+  var item = (this.name.includes("Pie")) ? pie : cake
   setTimeout(function() {
-    this.decorate(updateFunction)
+    item.decorate(updateFunction)
   }, 2000)
+  updateFunction(status)
 }
 
 function makeDessert() {
   //add code here to decide which make... function to call
   //based on which link was clicked
+  var pieNode = document.getElementById("pie")
+  var cakeNode = document.getElementById("cake")
+
+  if(this == cakeNode.getElementsByClassName('js-make')[0]){
+    makeCake()
+  }
+  else if(this == pieNode.getElementsByClassName('js-make')[0]){
+    makePie()
+  }
 }
 
 function serve(message, customer) {
